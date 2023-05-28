9e648d558dfd6b4ee0477c234d09260eb303f858
NoelSoria <noelsoria@gmail.com>
2023-05-27 22:19:20 -0300

    M04 Ejercicios resueltos

--- M03 JavaScript Fundamentos/Ejercicio 05.js
+++ M03 JavaScript Fundamentos/Ejercicio 05.js
@@ -21,40 +21,54 @@ function agregarSimboloExclamacion(str) {
    // Agrega un símbolo de exclamación al final del string "str" y retórnalo
    // Ejemplo: "hello world" ---> "hello world!"
    // Tu código:
+   let str2 = str+'!';
+   return(str2);
 }
 
 function combinarNombres(nombre, apellido) {
    // Retorna "nombre" y "apellido" combinados en un mismo string pero separados por un espacio.
    // Ejemplo: ("Soy", "Henry") ---> "Soy Henry"
    // Tu código:
+   let nombreapell = (nombre+' '+apellido);
+   return(nombreapell);
 }
 
 function obtenerSaludo(nombre) {
    // Toma el string "nombre" y concatena otra string en la cadena para que tome la siguiente forma:
    // Ejemplo: "Martin" ---> "Hola Martin!"
    // Tu código:
-}
+   return("Hola "+nombre+"!");
+} //console.log(obtenerSaludo("Jose"));
 
 function obtenerAreaRectangulo(alto, ancho) {
    // Retornar el área de un rectángulo teniendo su altura y ancho.
    // Tu código:
+   let area = alto*ancho;
+   return(area);
 }
 
 function retornarPerimetro(lado) {
    // La función recibe como argumento la medida de un lado de un cuadrado.
    // Debes retornar su perímetro.
    // Tu código:
+   let per = lado*4;
+   return(per);
+
 }
 
 function areaDelTriangulo(base, altura) {
    // Calcula el área de un triángulo y retorna el resultado.
    // Tu código:
+   let area = (base*altura)/2;
+   return(area);
 }
 
 function deEuroAdolar(euro) {
    // Supongamos que 1 euro equivale a 1.20 dólares.
    // Debes calcular el valor recibido como argumento pasándolo a dolares.
    // Tu código:
+   let dolar = euro*1.20;
+   return(dolar);
 }
 
 function esVocal(letra) {
@@ -62,6 +76,25 @@ function esVocal(letra) {
    // Si el usuario ingresa un string de más de un caracter debes retornar el mensaje: "Dato incorrecto".
    // Si no es vocal, tambien debe retornar "Dato incorrecto".
    // Tu código:
+   let vocal ='Es vocal';
+   let novocal= 'Dato incorrecto';
+   if (letra==='a') {
+      return(vocal);
+   }
+   else if (letra==='e'){
+      return(vocal);
+   }
+   else if (letra==='i'){
+      return(vocal);
+   }
+   else if(letra==='o'){
+      return(vocal);
+   }
+   else if(letra==='u'){
+      return(vocal);
+   }
+   else return(novocal);
+   
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
